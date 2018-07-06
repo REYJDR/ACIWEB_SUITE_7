4bb02861a782e9fd17e93cd7fe241db9477aa98c
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2018-07-06 08:38:52 -0500

    update

--- application/view/_templates/mob_header.php
+++ application/view/_templates/mob_header.php
@@ -13,7 +13,7 @@
                 <a href="#" data-panel="left" class="open-panel"><img src="i<?php echo URL; ?>mobile_assets/mages/icons/white/menu.png" alt="" title="" /></a>
           </div>	-->		
       <div class="navbar_right">
-       <a href="#" data-panel="right" class="open-panel"><img src="<?php echo URL; ?>moile_assets/images/icons/white/user.png" alt="" title="" /></a>
+       <a href="#" data-panel="right" class="open-panel"><img src="<?php echo URL; ?>mobile_assets/images/icons/white/user.png" alt="" title="" /></a>
       </div>
       
   </div>
