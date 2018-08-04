8c7696cf7002cb627d689af3296c1b4f303c3863
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2018-08-04 08:53:37 -0500

    update

--- application/controller/ges_ventas.php
+++ application/controller/ges_ventas.php
@@ -265,7 +265,7 @@ public function ges_print_OrdEmpaque($id){
             // load views
             require APP . 'view/_templates/header.php';
             require APP . 'view/_templates/panel.php';
-            require APP . 'view/operaciones/ges_print_OrdEmpaque.php';
+            require APP . 'view/modules/sales/SalesOrderEmpaque.php';
             require APP . 'view/_templates/footer.php';
 
 
