92fba3db23c2355f34bf4c31a9cc8b45b557ef4c
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2018-09-10 12:04:53 -0500

    update

--- public/js/operaciones/requisiciones/req_pago.js
+++ public/js/operaciones/requisiciones/req_pago.js
@@ -78,62 +78,20 @@ table.yadcf(
 
 });
 
-var table = $("#table").dataTable({
-  
-  responsive: false,
-  pageLength: 10,
-  dom: "Brtip",
-  bSort: false,
-  select: false,
 
-  info: false,
-    buttons: [
-      {
-      extend: "excelHtml5",
-      text:   "Exportar a Excel",
-      title:  "Estado_de_requisiciones",
-       
-      exportOptions: {
-            columns: ":visible",
-             format: {
-                header: function ( data ) {
-                  var StrPos = data.indexOf("<div");
-                    if (StrPos<=0){
-                      
-                      var ExpDataHeader = data;
-                    }else{
-                   
-                      var ExpDataHeader = data.substr(0, StrPos); 
-                    }
-                   
-                  return ExpDataHeader;
-                  }
-                }
-             
-              }
-            
-      }]
-
-});
 
 var table_fact = $("#table_fact").dataTable({
   
   responsive: false,
   pageLength: 10,
-  dom: "Brtip",
-  bSort: false,
-  select: false
-
+  
 });
 
 var table_cash = $("#table_cash").dataTable({
   
   responsive: false,
   pageLength: 10,
-  dom: "Brtip",
-  bSort: false,
-  select: false
-
+  
 });
 
 
