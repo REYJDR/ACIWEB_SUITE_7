79f3f9597ef65db549f6c69dc8610d8b8ab74795
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2018-09-17 18:30:40 -0500

    update

--- application/controller/ges_notasdecredito.php
+++ application/controller/ges_notasdecredito.php
@@ -724,7 +724,8 @@ private function CreateHeaderFile($id){
             $CustAddress.'|'.//Direccion
             $Total.'|'.      //Total Neto
             $Tax.'|'.        //Alicuota
-            $Motivo.'|'.     //NOTA MOTIVO
+          //  $Motivo.'|'.     //NOTA MOTIVO
+            '|'.               //NOTA MOTIVO
             $dfecha.'|'.     //FECHA DEVOLUCIONj
             $serprint.'|'.   //Serial de la impresora fiscal
             $NoInvoice.'|'.  //No. de factura fiscal
