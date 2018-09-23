15cad7c9b05864f0597d61985a5106a2927faf74
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2018-09-23 18:38:30 -0500

    UPDATE

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,17 @@
-var cacheName = 'ACIWEB APP';
 
-
- self.addEventListener('activate', function(event) {
-    console.log('[ServiceWorker] Activated');
-    event.waitUntil(
+  var cacheName = 'ACIWEB APP';
+  var filesToCache = ['/',
+  '/application',
+  '/application/view/mobile/',
+  '/public',
+  '/public/mobile_assets/'];
+  
+  self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
       caches.open(cacheName).then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/application',
-          '/application/view/mobile/',
-          '/public',
-          '/public/mobile_assets/'
-        ]);
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(filesToCache);
       })
     );
   });
-
-  
