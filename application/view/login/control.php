ef1b5e32f60ccaf9a7312e717255114e958a645c
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2018-10-30 10:02:19 -0600

    Update application/view/login/control.php

--- application/view/login/control.php
+++ application/view/login/control.php
@@ -85,13 +85,14 @@ if(isset($_POST['flag']))
 				</form>
 				<div class="separador col-lg-12"> </div>
 				<div class="col-lg-12">
-					<div class="col-lg-1"></div>
+				<!--	<div class="col-lg-1"></div>
 					<span id="siteseal"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=zB3xB13v2RV55GbgMLeYTlZOfhe0M5wMGtv2IKpi6SMS8b03XF39KAqppsrq"></script></span>
-					</div>
-					<div class="col-lg-4"></div>
+					</div> -->
+					<div class="separador col-lg-12"> </div>
+					<div class="col-lg-1"></div>
 						<span ><?php echo VER; ?></span>
 					</div>
-					<div class="separador col-lg-12"> </div>
+					
 			 </div>
 		</div>
 	</div>
