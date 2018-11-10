8d62b16ad6068e72cad88870f92e79f177467e7d
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2018-11-10 12:49:25 -0600

    Update application/view/modules/inventory/invIn.php

--- application/view/modules/inventory/invIn.php
+++ application/view/modules/inventory/invIn.php
@@ -264,7 +264,7 @@ require_once APP.'view/modules/'.basename(__DIR__).'/lang/'.$this->model->lang.'
 
 											<?PHP }else{ ?>
 
-											<select class="selectItems select col-lg-12"  id="vendorID" >
+											<select class="select col-lg-12"  id="vendorID" >
 											<option value="-" selected>-</option>
 											</select>
 
