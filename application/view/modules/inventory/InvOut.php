2a00b52b5fa6bf221edaa73c9885f08a05eabada
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2018-11-10 12:20:20 -0600

    Update application/view/modules/inventory/InvOut.php

--- application/view/modules/inventory/InvOut.php
+++ application/view/modules/inventory/InvOut.php
@@ -108,6 +108,7 @@ echo '<input type="hidden" id="FAC_NO_LINES" value="'.$NO_LINES .'" />';
 				<!--		<th width="5%"  class="text-center"><?php echo $TblLote?></th> -->
 				<!--		<th width="5%"  class="text-center"><?php echo $TblLoc;?></th> -->
 			        	<th width="5%"  class="text-center"><?php echo $TblUnitMeasure;?></th>
+						<th width="5%"  class="text-center"><?php echo $TblQtyOrd;?></th>
 						<th width="5%"  class="text-center"><?php echo $TblQty;?></th>
 				<!--		<th width="5%"  class="text-center"><?php echo $TblUnitP;?></th>-->
 				<!--		<th width="5%"  style="<?php echo $display;?>" class="text-center"><?php echo $TblTotal;?></th>-->
