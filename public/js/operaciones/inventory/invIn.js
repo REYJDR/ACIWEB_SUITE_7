53ff450615c8555cf3b3dd4ad6b3b12e645d0239
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2018-11-10 20:25:49 -0600

    Update public/js/operaciones/inventory/invIn.js

--- public/js/operaciones/inventory/invIn.js
+++ public/js/operaciones/inventory/invIn.js
@@ -1330,9 +1330,9 @@ function getOCitem(oc){
         url: link,
         data: {url: 'ges_compras/PO_item/'+oc},
         success: function(res){
-
-            console.log(res);
-          /*  json = JSON.parse(res);
+            
+            console.log(JSON.stringify(res));
+          /* 
             for(var i=0;i<res.length;i++){
                 console.log("<tr><td>"+res[i].ID_compania+"</td><td>"+res[i].Items_id+"</td></tr>");
                 }*/
