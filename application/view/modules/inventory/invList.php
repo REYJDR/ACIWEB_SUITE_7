a31bc4d7d9bf3455fc4d26d6bc7d12b2add904f2
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2018-11-21 17:46:23 -0600

    Update invList.php

--- application/view/modules/inventory/invList.php
+++ application/view/modules/inventory/invList.php
@@ -10,13 +10,14 @@ require_once APP.'view/modules/'.basename(__DIR__).'/lang/'.$this->model->lang.'
 <div class="limiter">
 		<div class="container-page100">
 			<div class="wrap-page100">
+
+			<div class="separador col-lg-12"> </div>
+
+			<form class="login100-form validate-form p-l-25 p-r-25 p-t-60" >
+			
 			<div class="col-lg-2">
 			<button  class="btn-bar" name="search" onclick="getListItem()"><?php echo $InvListButtom; ?></button>
 			</div>
-			<div class="separador col-lg-12"> </div>
-
-			<form method="POST"  action="" class="login100-form validate-form p-l-25 p-r-25 p-t-60" >
-
 			<span class="page100-form-title">
 						<?PHP echo $Title1; ?>
 			</span>
