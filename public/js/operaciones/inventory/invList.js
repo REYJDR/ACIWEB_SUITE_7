f772787b97c7c15815e70c1d08668dab99970994
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2018-11-22 23:06:01 -0600

    Update invList.js

--- public/js/operaciones/inventory/invList.js
+++ public/js/operaciones/inventory/invList.js
@@ -50,7 +50,7 @@ function getListItem(){
          $('#listItem').html(''); 
          $('#listItem').append(res);
 
-          table.ajax.reload();
+          table.reload();
         //  table.clear().draw();
    
          /*   for(var m=0;m<res.length;m++){
