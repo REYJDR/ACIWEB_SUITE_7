64338ba2b38692f2b52a6e9faaba3b33a22e7004
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2018-12-01 12:48:50 -0600

    test

--- application/view/modules/inventory/invList.php
+++ application/view/modules/inventory/invList.php
@@ -10,20 +10,22 @@ require_once APP.'view/modules/'.basename(__DIR__).'/lang/'.$this->model->lang.'
 <div class="limiter">
 		<div class="container-page100">
 			<div class="wrap-page100">
-
+		
 			<div class="separador col-lg-12"> </div>
 
 			<!--	<form method="POST" action="" class="login100-form validate-form p-l-25 p-r-25 p-t-60" > -->
 			<div class="login100-form validate-form p-l-25 p-r-25 p-t-60" > 
+				<!--INI DIV ERRO-->
+				<div id="ERROR" class="alert"></div>
+				<div class="separador col-lg-12"> </div>
+			<!--INI DIV ERROR-->
 			<div class="col-lg-2">
 			<button  class="btn-bar" name="searchItem" id="searchItem"  onclick="getListItem()"><?php echo $InvListButtom; ?></button>
 			</div>
 			<span class="page100-form-title">
 						<?PHP echo $Title1; ?>
 			</span>
-			<!--INI DIV ERRO-->
-			<div id="ERROR" class="alert"></div>
-			<!--INI DIV ERROR-->
+			
 			
 			<!--ini contenido-->
 			<div class="col-lg-12"> 
