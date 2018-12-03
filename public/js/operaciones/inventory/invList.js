c46346134ae99683ba6e418e93e6e3714aded66f
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2018-12-02 18:38:50 -0600

    update

--- public/js/operaciones/inventory/invList.js
+++ public/js/operaciones/inventory/invList.js
@@ -86,7 +86,7 @@ function getListItem(){
 
         console.log('data: '+ data);
 
-        printTbl.rows.add( data ); 
+        printTbl.rows.add( data ).draw(); 
 
        /* for(var m=0;m<items.length;m++){
                             
@@ -104,7 +104,7 @@ function getListItem(){
 
         } */
 
-        printTbl.draw();
+      
         MSG_ERROR_RELEASE();
         MSG_CORRECT("Done",0);
 
