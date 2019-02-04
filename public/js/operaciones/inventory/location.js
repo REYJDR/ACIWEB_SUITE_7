51c12b9cef3ced27bfa7b7b6f6ab7dd17790d90f
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2019-02-04 10:28:34 -0600

    update

--- public/js/operaciones/inventory/location.js
+++ public/js/operaciones/inventory/location.js
@@ -183,7 +183,7 @@ function  getItemList(location,stock){
             MSG_ERROR_RELEASE();
             MSG_ADVICE("Loading items...",0);
             addRows(res);
-            $('#item').show();
+            
         }
 
     });
@@ -204,6 +204,7 @@ function addRows(items) {
 
     MSG_ERROR_RELEASE();
     MSG_CORRECT("Done",0);
+    $('#item').show();
 
 }
 
