bb5515fb57eec36361ed17fcc43443ecf43c2b17
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2019-04-17 13:02:40 -0500

    update

--- public/js/operaciones/ventas/SalesOrderSto.js
+++ public/js/operaciones/ventas/SalesOrderSto.js
@@ -92,8 +92,8 @@ while(i <= cantLineas){
        }        
 
       var line_table_req = '<tr>'+reglon+
-      "<td width='15%' class='rowtable_req' onkeypress='MSG_ERROR_RELEASE();' onkeyup='checkTblChar(this.id); checkLong(this.id,"+40+");' contenteditable id='desc"+i+"'  ></td>"+
-      "<td width='15%' class='rowtable_req' onkeypress='MSG_ERROR_RELEASE();' onkeyup='checkTblChar(this.id); checkLong(this.id,"+10+");' contenteditable id='nota"+i+"'  ></td>"+
+      "<td width='15%' class='rowtable_req' onkeypress='MSG_ERROR_RELEASE();' onkeyup='checkTblChar(this.id); checkLong(this.id,40);' contenteditable id='desc"+i+"'  ></td>"+
+      "<td width='15%' class='rowtable_req' onkeypress='MSG_ERROR_RELEASE();' onkeyup='checkTblChar(this.id); checkLong(this.id,10);' contenteditable id='nota"+i+"'  ></td>"+
       '<input type="hidden"   id="unit'+i+'" />'+
       '<td width="3%" style="'+displayStock+'"  class="rowtable_req numb" id="Tblote'+i+'"  ></td>'+
       '<input type="hidden"  id="taxable'+i+'" />'+
