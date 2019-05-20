5b8d4e05746daa159e90f8f1d625617a59efa019
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2019-05-20 18:10:52 -0500

    update

--- application/controller/ges_inventario.php
+++ application/controller/ges_inventario.php
@@ -1633,7 +1633,7 @@ public function getStocLockName($id){
     
         $clause = ' LEFT JOIN STOCK_LOCATION AS B ON  A.id = A.Location
                     LEFT JOIN STOCKS AS C on C.ID = A.stock 
-                    WHERE A.id=$id';
+                    WHERE A.id="$id"';
     
         $Item= $this->model->queryColumns('STOCK_ITEMS_LOCATION as A', $columns,$clause);
 
