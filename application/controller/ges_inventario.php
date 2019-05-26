cf0f8c8e942f1e12c79931dc6549b32c0197e57d
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2019-05-26 18:34:10 -0500

    update

--- application/controller/ges_inventario.php
+++ application/controller/ges_inventario.php
@@ -1726,7 +1726,7 @@ public function getItemsStocksList(){
                     LEFT JOIN STOCK_LOCATION  AS B ON  B.id = A.Location
                     LEFT JOIN STOCKS AS C on C.ID = A.stock
                     
-                WHER D.ID_compania="'.$this->model->id_compania.'"';
+                WHER D.id_compania="'.$this->model->id_compania.'"';
     
        $Item= $this->model->queryColumns('Products_Exp as D', $columns,$clause);
     
