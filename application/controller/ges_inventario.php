5b3e7e86a869ec128ad15fb6c6e7f856ce2b1b73
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2019-10-21 11:15:50 -0500

    update

--- application/controller/ges_inventario.php
+++ application/controller/ges_inventario.php
@@ -707,7 +707,7 @@ public function getLocationByItem(){
             $qty="'".$STATUS_LOC->{'qty'}."'";
             $url="'".URL."'";
 
-          echo  $qtypend = $this->model->Query_value('sale_pendding','sum(qty)','where ProductID="'.$this->ProductID.'" and status_pendding <> "1" and status_location_id="'.$ID_STATUS.'" and ID_compania="'.$this->model->id_compania.'"');
+            $qtypend = $this->model->Query_value('sale_pendding','sum(qty)','where ProductID="'.$this->ProductID.'" and status_pendding <> "1" and status_location_id="'.$ID_STATUS.'" and ID_compania="'.$this->model->id_compania.'"');
             
         
             //  if($pendding_sale>0 || $STOCK_QTY>0 ){
