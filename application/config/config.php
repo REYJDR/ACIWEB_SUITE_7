29ed28ab6f35d7108d83739a7060e8ad772d930f
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2020-05-11 23:09:03 -0500

    url customer

--- application/config/config.php
+++ application/config/config.php
@@ -61,7 +61,7 @@ if (isset($_SERVER['HTTPS'])){
 define('URL_DOMAIN', $_SERVER['HTTP_HOST']);
 define('URL_SUB_FOLDER', str_replace(URL_PUBLIC_FOLDER, '', dirname($_SERVER['SCRIPT_NAME'])));
 define('URL', str_replace('\\', '/', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER));
-define('URL_CUSTOMER', str_replace('\\', '/', URL_PROTOCOL . URL_DOMAIN ));
+define('URL_CUSTOMER', str_replace('\\', '/', URL_PROTOCOL . URL_DOMAIN . dirname($_SERVER['SCRIPT_NAME']));
 
 
 define('VER','v3.0.2.0');
