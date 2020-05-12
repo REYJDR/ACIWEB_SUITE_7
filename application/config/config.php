f5f65c3a55e75bdbf73f1a04f8ff2c5b54bd573a
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2020-05-11 23:38:49 -0500

    update

--- application/config/config.php
+++ application/config/config.php
@@ -62,7 +62,7 @@ define('URL_DOMAIN', $_SERVER['HTTP_HOST']);
 define('URL_SUB_FOLDER', str_replace(URL_PUBLIC_FOLDER, '', dirname($_SERVER['SCRIPT_NAME'])));
 define('URL', str_replace('\\', '/', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER));
 
-list($cusomer_dir,$repository) = explode('/',URL_SUB_FOLDER);
+list($cusomer_dir,$repository) = explode('\/',URL_SUB_FOLDER);
 
 define('URL_STORE', str_replace('\\', '/', URL_PROTOCOL . URL_DOMAIN . $cusomer_dir));
 
