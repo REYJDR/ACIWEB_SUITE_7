add0fd9c851bb8681f1017c941fdbad936ff4dfb
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2020-05-14 23:32:33 -0500

    update

--- application/controller/bridge_query.php
+++ application/controller/bridge_query.php
@@ -2542,7 +2542,7 @@ public function do_curl_request($api_user,$api_url,$api_token,$api_route,$data) 
   //get token
   $token = $this->get_token($api_user,$api_token );
 
-
+die($token);
   $url = $api_url.'/index.php?route='.$api_route.'&api_token='.$token;
 
 
