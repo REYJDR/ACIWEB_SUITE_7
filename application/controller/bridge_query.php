9e5fc49f53bd504684c12aa095389f7372748cb9
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2020-05-15 12:51:40 -0500

    update

--- application/controller/bridge_query.php
+++ application/controller/bridge_query.php
@@ -2461,7 +2461,7 @@ public function oc_getOrders(){
 
   $response = $this->do_curl_request('Default',$_GET['api_url'] ,$_GET['api_key'] ,$_GET['api_route'],json_encode($json, JSON_PRETTY_PRINT));
 
-
+var_dump($response);
   foreach($response as $key => $value){
     
     if(is_object($value)){
