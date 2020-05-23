2b5abecdaf4321df9fbc9378bd911e177e2c9370
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2020-05-23 14:20:32 -0500

    update

--- application/controller/bridge_query.php
+++ application/controller/bridge_query.php
@@ -2873,9 +2873,9 @@ public function do_curl_request($api_user,$api_url,$api_token,$api_route,$data) 
 
   $response = curl_exec($ch);
   curl_close($ch);
-
+  die$response();   
   $response = json_decode($response);
-   
+ 
   if($response == ''|| $response->code >= '400'){
     exit(json_encode(array('Warning' => $response)));
   }
