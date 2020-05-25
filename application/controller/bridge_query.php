462a275c35bacb3fce1d4e97b17510f733a240f1
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2020-05-24 19:00:24 -0500

    update

--- application/controller/bridge_query.php
+++ application/controller/bridge_query.php
@@ -2815,7 +2815,8 @@ public function get_token($api_url,$api_token){
 
   $url = $api_url.'/index.php?route=api/login';
 
-  $params = array("key" => $api_token)
+  $params = array("key" => $api_token);
+
   $ch = curl_init();
   curl_setopt($ch,CURLOPT_URL, $url);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -2852,47 +2853,6 @@ public function get_token($api_url,$api_token){
          }
     return $response->{'api_token'} ;
 
-
-
-
-
-    $curl = curl_init();
-
-    $options =  array(
-      CURLOPT_URL => $url,
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 0,
-      CURLOPT_FOLLOWLOCATION => true,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_POST => true,
-      CURLOPT_POSTFIELDS =>  http_build_query(array("key" => $api_token)),
-      CURLOPT_HTTPHEADER => array(
-        "Content-Type: application/json",
-        "Content-Type: application/x-www-form-urlencoded"
-        
-        
-     )
-    );
-
-    curl_setopt_array($curl, $options);
-    $response = curl_exec($curl);
-    curl_close($curl);
-
-    $response = json_decode($response);
-
-       if($response->{'error'}){
-     
-         foreach($response->{'error'} as $key => $value){
-     
-           echo '['.$key.']['.$value.']<br>';
-     
-         }
-     
-         die();
-       }
-     return $response->{'api_token'} ;
 }
 
 
