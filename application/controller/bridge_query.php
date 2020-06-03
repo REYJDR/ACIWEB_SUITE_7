098e01a313ca3075b9c0a5801fa45cd4981ad990
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2020-06-03 16:34:45 -0500

    update

--- application/controller/bridge_query.php
+++ application/controller/bridge_query.php
@@ -2813,6 +2813,10 @@ public function oc_getTblCol(){
   
   $json = ['product','product_description'];
 
+  die( $_GET['api_url'].
+  $_GET['api_key'].
+  $_GET['api_route']);
+
   $response = $this->do_curl_request('Default',$_GET['api_url'] ,$_GET['api_key'] ,$_GET['api_route'],json_encode($json, JSON_PRETTY_PRINT));
 
   var_dump($response);
