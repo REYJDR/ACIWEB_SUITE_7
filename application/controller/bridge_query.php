5445f7da58fd14d874e89d587fffbfd4d25e64a8
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2020-06-03 16:44:30 -0500

    update

--- application/controller/bridge_query.php
+++ application/controller/bridge_query.php
@@ -2815,7 +2815,7 @@ public function oc_getTblCol(){
 
   $response = $this->do_curl_request('Default',$_GET['api_url'] ,$_GET['api_key'] ,$_GET['api_route'],json_encode($json, JSON_PRETTY_PRINT));
 
-
+ var_dump($response);
   foreach($response as $key => $value){
     
     if(is_array($value)){
