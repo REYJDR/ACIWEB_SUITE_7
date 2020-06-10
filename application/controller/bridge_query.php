f5c3d6c1b800332c20f0ae4dbfed6fb064d28f64
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2020-06-10 12:14:50 -0500

    update

--- application/controller/bridge_query.php
+++ application/controller/bridge_query.php
@@ -2713,7 +2713,7 @@ public function oc_setItems() {
     
   }
   
- die(json_encode($json[1], JSON_PRETTY_PRINT)); 
+ //die(json_encode($json[1], JSON_PRETTY_PRINT)); 
 
   //Execute curl
   $response = $this->do_curl_request('Default',$_GET['api_url'] ,$_GET['api_key'] ,$_GET['api_route'],json_encode($json, JSON_PRETTY_PRINT),'POST');
