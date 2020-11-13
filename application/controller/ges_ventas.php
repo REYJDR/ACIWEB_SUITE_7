fead438f2940b884e64e88d0afc433756aeccbc5
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2020-11-13 14:14:49 -0600

    up

--- application/controller/ges_ventas.php
+++ application/controller/ges_ventas.php
@@ -51,7 +51,7 @@ public function mob_orden_ventas(){
 
           if($res=='0'){
             require APP . 'view/_templates/mob_header.php'; 
-            echo = '<div class="modal fade" id="procesing" tabindex="-1" role="dialog" aria-labelledby="procesing" aria-hidden="true">
+            echo  '<div class="modal fade" id="procesing" tabindex="-1" role="dialog" aria-labelledby="procesing" aria-hidden="true">
               <div class="modal-dialog modal-dialog-centered" role="document">
                   <div class="modal-content">
            
