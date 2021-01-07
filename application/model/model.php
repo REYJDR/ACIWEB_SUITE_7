5028a364ff0ae07c657878d29f772af68141b711
DAOUTEL <REINALDO.DAOU@GMAIL.COM>
2021-01-06 21:43:30 -0600

    test

--- application/model/model.php
+++ application/model/model.php
@@ -265,6 +265,8 @@ public function queryColumns($table,$columns,$clause){
 
     $query = "SELECT  ".implode(',', $columns)." FROM ".$table." ".$clause." ;";
 
+
+    echo $query;
     //file_put_contents("LOG_ERROR/test.log",$query ); 
     return $this->Query($query);
         
