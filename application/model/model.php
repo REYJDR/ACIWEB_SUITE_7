9371083787c5d0a1defb336d6efa18fb4d20b27d
Reinaldo Daou <reyjdaou@MacRey.local>
2021-07-09 11:51:47 -0500

    upda

--- application/model/model.php
+++ application/model/model.php
@@ -378,7 +378,7 @@ $_SESSION['ROLE2'] = $rol_campo;
 $_SESSION['CANCLOSE'] = $can_close;
 $_SESSION['COMPANY'] = $company;
 
-//session_write_close();
+echo SID; die();
 
 if($temp_url!=''){
 
@@ -416,7 +416,7 @@ if($temp_url!=''){
                                             echo '<script>
                                                 console.log("url'.$url.'");
                                                 alert("Advertencia: El sistema se encuentra desconectado de SageConnect, Por favor verificar");
-                                                self.location="'.URL.'index.php?url=home/index/"'.SID.';
+                                                self.location="'.URL.'index.php?url=home/index";
                                                 </script>';
                                                 // self.location="'.URL.'index.php?url=home/index";
                                                // header('Location: '.URL.'index.php?url=home/index');
